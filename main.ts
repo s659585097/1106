1fadf6939ebb4af1c49cc7907ebdff46a38569d3
s659585097 <67514298+s659585097@users.noreply.github.com>
2020-11-13 11:49:38 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
 serial.redirectToUSB()
 basic.forever(function () {
-    serial.writeLine("" + pins.analogReadPin(AnalogPin.P0) + "---" + pins.analogReadPin(AnalogPin.P1))
-    basic.pause(100)
-    if (pins.analogReadPin(AnalogPin.P1) > 550) {
+    serial.writeLine("" + pins.analogReadPin(AnalogPin.P1) + "---" + pins.analogReadPin(AnalogPin.P2))
+    if (pins.analogReadPin(AnalogPin.P1) > 400) {
         robotbit.MotorRunDual(
         robotbit.Motors.M1A,
         160,
         robotbit.Motors.M2B,
         20
         )
-    } else if (pins.analogReadPin(AnalogPin.P2) > 550) {
+    } else if (pins.analogReadPin(AnalogPin.P2) > 400) {
         robotbit.MotorRunDual(
         robotbit.Motors.M1A,
         20,
