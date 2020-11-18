d6bb40d0b6fe20e2c8c3a0fd35e4a570c80dc737
s659585097 <67514298+s659585097@users.noreply.github.com>
2020-11-18 17:55:12 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,42 @@
-let 障礙 = 0
 serial.redirectToUSB()
 basic.forever(function () {
+    serial.writeLine("" + pins.analogReadPin(AnalogPin.P1) + "---" + pins.analogReadPin(AnalogPin.P2))
     basic.showNumber(Math.round(sensors.sensor_ping(
     DigitalPin.P8,
     DigitalPin.P0,
     sensors.PingUnit.Centimeters
     )))
-    障礙 = 0
     if (sensors.sensor_ping(
     DigitalPin.P8,
     DigitalPin.P0,
     sensors.PingUnit.Centimeters
-    ) > 2 && sensors.sensor_ping(
+    ) > 5 && sensors.sensor_ping(
     DigitalPin.P8,
     DigitalPin.P0,
     sensors.PingUnit.Centimeters
-    ) < 10) {
-        障礙 = 1
+    ) < 15) {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        255,
+        robotbit.Motors.M2B,
+        0
+        )
+        basic.pause(200)
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        80,
+        robotbit.Motors.M2B,
+        80
+        )
+        basic.pause(500)
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        0,
+        robotbit.Motors.M2B,
+        255
+        )
+        basic.pause(200)
     } else {
-    	
-    }
-})
-basic.forever(function () {
-    serial.writeLine("" + pins.analogReadPin(AnalogPin.P1) + "---" + pins.analogReadPin(AnalogPin.P2))
-    if (障礙 == 0) {
         if (pins.analogReadPin(AnalogPin.P1) > 300) {
             robotbit.MotorRunDual(
             robotbit.Motors.M1A,
@@ -38,8 +51,6 @@ basic.forever(function () {
             robotbit.Motors.M2B,
             255
             )
-        } else if (false) {
-        	
         } else {
             robotbit.MotorRunDual(
             robotbit.Motors.M1A,
@@ -48,27 +59,5 @@ basic.forever(function () {
             80
             )
         }
-    } else {
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
-        255,
-        robotbit.Motors.M2B,
-        0
-        )
-        basic.pause(1000)
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
-        80,
-        robotbit.Motors.M2B,
-        80
-        )
-        basic.pause(5000)
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
-        0,
-        robotbit.Motors.M2B,
-        255
-        )
-        basic.pause(1000)
     }
 })
